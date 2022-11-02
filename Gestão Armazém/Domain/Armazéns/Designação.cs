1690ecd79daf1eaf4f97455fbaa95da03ff0cd84
1190624 <1190624@isep.ipp.pt>
2022-11-02 10:26:06 +0000

    Modificação class "Designação"
    LEI-26 #comment Modificação class "Designação".

--- Gestão Armazém/Domain/Armazéns/Designação.cs
+++ Gestão Armazém/Domain/Armazéns/Designação.cs
@@ -1,9 +1,16 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Serialization;
 using DDDSample1.Domain.Shared;
 
 namespace DDDSample1.Domain.Armazéns {
+    [Owned]
+    [DataContract]
     public class Designação : IValueObject {
+        [DataMember(Name = "Texto")]
         private String texto;
+
+        public Designação() {}
 
         public Designação(String texto) {
             this.texto = texto;
