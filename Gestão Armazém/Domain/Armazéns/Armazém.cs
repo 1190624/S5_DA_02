62f971bbe8ea6f51b044d17e8d5a4f3ea2d8344b
1190624 <1190624@isep.ipp.pt>
2022-11-02 10:24:47 +0000

    Modificação class "Armazém"
    LEI-26 #comment Modificação class "Armazém".

--- Gestão Armazém/Domain/Armazéns/Armazém.cs
+++ Gestão Armazém/Domain/Armazéns/Armazém.cs
@@ -1,25 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using DDDSample1.Domain.Shared;
 using DDDSample1.Domain.Common;
 
 namespace DDDSample1.Domain.Armazéns {
+    [DataContract]
     public class Armazém : Entity<Identificador>, IAggregateRoot {
+        [Key]
+        [DataMember(Name = "Identificador")]
+        private Identificador identificador;
+        [DataMember(Name = "Designação")]
         private Designação designação;
+        [DataMember(Name = "Endereço")]
         private Endereço endereço;
+        [DataMember(Name = "Munícipio")]
         private Munícipio munícipio;
+        [DataMember(Name = "Coordenadas")]
         private Coordenadas coordenadas;
 
         public Armazém() {}
 
         public Armazém(Identificador identificador, Designação designação, Endereço endereço, Munícipio munícipio, Coordenadas coordenadas) {
-            this.Id = identificador;
+            this.identificador = identificador;
             this.designação = designação;
             this.endereço = endereço;
             this.munícipio = munícipio;
             this.coordenadas = coordenadas;
         }
 
+        public Armazém(Identificador identificador, Designação designação) {
+            this.identificador = identificador;
+            this.designação = designação;
+        }
+
+        public Identificador Identificador => identificador;
         public Designação Designação => designação;
         public Endereço Endereço => endereço;
+        public Endereço GetEndereço() {
+            return endereço;
+        }
         public Munícipio Munícipio => munícipio;
         public Coordenadas Coordenadas => coordenadas;
     }
