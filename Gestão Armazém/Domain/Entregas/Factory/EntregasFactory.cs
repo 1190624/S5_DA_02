8f130c9885e43fa2618a493bbba1687e5a55bf96
1190825 Luís Costa <1190825@isep.ipp.pt>
2022-11-03 22:31:25 +0000

    Criação da classe "EntregasFactory"
    LEI-39 LEI-43 #comment Commit EntregasFactory class

---
+++ Gestão Armazém/Domain/Entregas/Factory/EntregasFactory.cs
@@ -0,0 +1,29 @@
+using System;
+using DDDSample1.Domain.Entregas.Validator;
+using DDDSample1.Domain.Common;
+
+namespace DDDSample1.Domain.Entregas.Factory {
+    public interface EntregasFactory : IEntregasFactory {
+        public Entrega CriarEntrega(String identificador,
+        String dia,
+        String mes,
+        String ano,
+        Double massa,
+        Double tempoColocação,
+        Double tempoRetirada
+        ){
+            EntregasValidator entregasValidator = new EntregasValidator();
+        
+            if(entregasValidator.IsValid(identificador,
+                dia,
+                mes,
+                ano,
+                massa,
+                tempoColocação,
+                tempoRetirada))
+                return new Entrega(new Identificador(identificador), new DataEntrega(dia,mes,ano), new Massa(massa), new TempoColocação(tempoColocação), new TempoRetirada(tempoRetirada));
+
+            return null;
+        }
+    }
+}
