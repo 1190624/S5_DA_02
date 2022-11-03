ec72d2e6bcd844f016aef3f6ea5871951ec49ba2
Luís Carlos <1190818@isep.ipp.pt>
2022-11-03 22:55:33 +0000

    Update Armazém.cs
    LEI-32 #comment Implementação de métodos para edição de Armazém

--- Gestão Armazém/Domain/Armazéns/Armazém.cs
+++ Gestão Armazém/Domain/Armazéns/Armazém.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 using DDDSample1.Domain.Shared;
 using DDDSample1.Domain.Common;
+using System;
 
 namespace DDDSample1.Domain.Armazéns {
     [DataContract]
@@ -41,5 +42,25 @@ namespace DDDSample1.Domain.Armazéns {
         }
         public Munícipio Munícipio => munícipio;
         public Coordenadas Coordenadas => coordenadas;
+
+        public void changeDesignação(String designação)
+        {
+            this.designação = new Designação(designação);
+        }
+
+        public void changeEndereço(String códigoPostal, Int16 númeroPorta, String nomeRua, String localidade, String país)
+        {
+            this.endereço = new Endereço(códigoPostal, númeroPorta, nomeRua, localidade, país);
+        }
+
+        public void changeMunicípio(String munícipio)
+        {
+            this.munícipio = new Munícipio(munícipio);
+        }
+
+        public void changeCoordenadas(Double latitude, Double longitude)
+        {
+            this.coordenadas = new Coordenadas(latitude, longitude);
+        }
     }
 }
