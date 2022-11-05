97799a20ebd357c20fc1106d3dd8b8f4ab7ed9b3
Lara Domingos <1190797@isep.ipp.pt>
2022-11-05 15:40:59 +0000

    LEI-47 #comment service class

---
+++ Gestão Armazém/Domain/Entregas/EntregaService.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DDDSample1.Domain.Shared;
+using DDDSample1.Domain.Common;
+using DDDSample1.Domain.Entregas.DTO;
+using System;
+using DDDSample1.Domain.Entregas;
+
+namespace DDDSample1.Domain.Armazéns
+{
+    public class EntregaService
+    {
+        private readonly IEntregasRepository _repo;
+
+        private readonly IUnitOfWork gestorPersist;
+
+        public EntregaService(IUnitOfWork gestorPersist, IEntregasRepository repo)
+        {
+            this.gestorPersist = gestorPersist;
+            this._repo = repo;
+        }
+
+        public async Task<EntregasDTO> editarEntregaAsync(EntregasDTO novaEntregaDTO)
+        {
+            var entrega = await this._repo.GetByIdAsync(new Identificador(novaEntregaDTO.GetIdentificador));
+            if(entrega == null)
+            return null;
+
+
+            //TODO verificar se existe armazem 
+            // change all fields
+            
+        /*
+            armazém.changeDesignação(novoArmazémDTO.GetDesignação);
+            armazém.changeEndereço(novoArmazémDTO.códigoPostal, novoArmazémDTO.GetNúmeroPorta, novoArmazémDTO.GetNomeRua,
+                novoArmazémDTO.GetLocalidade, novoArmazémDTO.GetPaís);
+            armazém.changeMunicípio(novoArmazémDTO.GetMunícipio);
+            armazém.changeCoordenadas(novoArmazémDTO.latitude, novoArmazémDTO.longitude);
+*/
+
+            await this.gestorPersist.CommitAsync();
+
+            return EntregasMapper.toDTO(entrega);
+        }
+    }
+}
