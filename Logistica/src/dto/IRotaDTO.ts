16af37b3c24ad58c2a3a921ef969616bd9b179b9
Luís Carlos <1190818@isep.ipp.pt>
2022-11-06 17:52:25 +0000

    Create IRotaDTO.ts
    LEI-66 #comment Criação Rota DTO

---
+++ Logistica/src/dto/IRotaDTO.ts
@@ -0,0 +1,9 @@
+export default interface IRotaDTO {
+    rotaId: string;
+    origem: string;
+    destino: string;
+    distancia: string;
+    tempo: string;
+    gastoEnergetico: string;
+    tempoCargaExtra: string;
+  }
