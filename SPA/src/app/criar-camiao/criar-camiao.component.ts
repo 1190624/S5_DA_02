71d2b15cb2a9b0cd1c8f0b9e200e00b94c54c015
Lara Domingos <1190797@isep.ipp.pt>
2022-11-22 16:16:55 +0000

    LEI-78 #comment: UC L.1.3 - criar camião component

--- SPA/src/app/criar-camiao/criar-camiao.component.ts
+++ SPA/src/app/criar-camiao/criar-camiao.component.ts
@@ -11,18 +11,18 @@ import { ReturnStatement } from '@angular/compiler';
   styleUrls: ['./criar-camiao.component.css']
 })
 export class CriarCamiaoComponent implements OnInit {
-  camiao!: Camiao;
-  matricula: string = ''; 
-  caracteristica: string = '';
-  autonomia:number = 0;
-  capacidadeTransporte:number = 0;
-  capacidadeBateria:number = 0;
-  tara: number = 0;
-  tempoCarregamento:string = '';
+  camiao: Camiao;
+  matricula: string; 
+  caracteristica: string;
+  autonomia:number;
+  capacidadeTransporte:number;
+  capacidadeBateria:number;
+  tara: number;
+  tempoCarregamento:string;
   
   
   
-  constructor(private formBuilder: FormBuilder, private service : CamiaoService, private route: ActivatedRoute, private router:Router) { }
+  constructor(private service : CamiaoService, private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -33,34 +33,27 @@ export class CriarCamiaoComponent implements OnInit {
       this.camiao = data}) */
     
       Submit(): void {
+
+          this.camiao = new Camiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte, 
+            this.capacidadeBateria, this.tara, this.tempoCarregamento);
+          this.service.criarCamiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte, 
+            this.capacidadeBateria, this.tara, this.tempoCarregamento).subscribe(data => { alert("O camião foi criado.") });
+        /*
         //Get inputs from the boxes
         var matricula = document.getElementById('matricula') as HTMLInputElement; 
-        var caracteristica = document.getElementById('caracteristica')as HTMLInputElement;
+        var caracteristica = document.getElementById('caracteristica') as HTMLInputElement;
         var autonomia = document.getElementById('autonomia') as HTMLInputElement;
         var capacidadeTransporte= document.getElementById('autonomia') as HTMLInputElement;
         var capacidadeBateria= document.getElementById('capacidadeTransporte') as HTMLInputElement;
         var tara= document.getElementById('tara') as HTMLInputElement;
         var tempoCarregamento= document.getElementById('tempoCarregamento') as HTMLInputElement;
 
-
-
-        var title = document.getElementById('title') as HTMLInputElement;
-        var description = document.getElementById('description') as HTMLInputElement;
-        var tags = document.getElementById('tags') as HTMLInputElement;
-    
-/*         //Check if the posts inputs are valid.
-        if (title.value.length < 1 || description.value.length < 1) {
-          alert("The post format is incorrect. Try again.");
-        } else {
-          //Getting the author of the post and the date
-          var author = this.playerName;
-          var date = new Date(); */
   
-           //Publish the post
+  /*
           this.camiao = new Camiao(matricula.value, caracteristica.value, autonomia.valueAsNumber, capacidadeTransporte.valueAsNumber, 
             capacidadeBateria.valueAsNumber, tara.valueAsNumber, tempoCarregamento.value);
           this.service.criarCamiao(this.camiao).subscribe(data => { alert("O camião foi criado.") });
-          
+          */
            
 /*           this.service.criarCamiao(this.formBuilder.group({
             matricula:matricula.value, 
@@ -76,7 +69,7 @@ export class CriarCamiaoComponent implements OnInit {
           title.value = '';
           tags.value = '';
           description.value = ''; */
-
+/*
           matricula.value='';
           caracteristica.value='';
           autonomia.valueAsNumber=0;
@@ -84,13 +77,14 @@ export class CriarCamiaoComponent implements OnInit {
           capacidadeBateria.valueAsNumber=0;
           tara.valueAsNumber=0;
           tempoCarregamento.value='';
+         */ 
           
         }
       
 
 
       Return(): void{
-        this.router.navigate(['/menuNav']);
+        this.router.navigate(['/appRoot']);
       }
   }
 
--- SPA/src/app/services/camiao.service.ts
+++ SPA/src/app/services/camiao.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,  HttpErrorResponse } from '@angular/common/http';
-import {catchError, map, Observable, throwError} from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Camiao } from '../model/camiao';
 
 @Injectable({
@@ -11,10 +11,41 @@ export class CamiaoService {
   constructor(private httpClient: HttpClient) { }
 
 
-  criarCamiao(camiao: Camiao): Observable<any>{
+  criarCamiao(matricula: string, caracteristica: string, autonomia:number, capacidadeTransporte:number, capacidadeBateria:number,
+    tara: number, tempoCarregamento:string): Observable<any> {
 
-    return this.httpClient.post(this.url, camiao).pipe(map(this.extractData));
-  
+    const MATRICULA_REGEX = new RegExp(/[A-Z]{2}-[0-9]{2}-[A-Z]{2}/);
+    //verificar se os valores do camião estão corretos.
+    if (autonomia < 90) {
+      alert("Automia do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 90).");
+    }
+
+    if (capacidadeBateria < 55) {
+      alert("Capacidade de Bateria do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 55).");
+    }
+
+    if (capacidadeTransporte < 800) {
+      alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
+    }
+
+    if (caracteristica == " ") {
+      alert("Necessário inserir uma Característica do Camião Elétrico!");
+    }
+
+    if (tara < 1000) {
+      alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
+    }
+
+    if (!MATRICULA_REGEX.test(matricula)) {
+      alert("Matrícula do Camião Elétrico não se apresenta no formato estipulado(formato exemplo = AA-00-ZZ).");
+    }
+
+    const body = {"matricula":matricula,"caracteristica":caracteristica,
+    "autonomia":autonomia, "capacidadeTransporte":capacidadeTransporte, "capacidadeBateria":capacidadeBateria, "tara": tara, "tempoCarregamento":tempoCarregamento}
+    
+    console.log(body);
+    return this.httpClient.post(this.url, body).pipe(map(this.extractData));
+
   }
   /*
   
@@ -29,22 +60,22 @@ export class CamiaoService {
   */
 
 
-  public extractData(res:any){
+  public extractData(res: any) {
     return res || {};
   }
 
 
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
-        // A client-side or network error occurred. Handle it accordingly.
-        console.error('Ocorreu um erro:', error.error);
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('Ocorreu um erro:', error.error);
     } else {
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong.
-        console.error(
-            `Backend returnou o código ${error.status}, menssagem: `, error.error);
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returnou o código ${error.status}, menssagem: `, error.error);
     }
     // Return an observable with a user-facing error message.
     return throwError(() => new Error('Aconteceu algo; por favor tente mais tarde'));
-}
+  }
 }
