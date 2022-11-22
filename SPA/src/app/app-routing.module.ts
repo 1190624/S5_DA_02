1ab11e396c1d3c8f05ef7c2436c3b91243bd8f2b
Lara Domingos <1190797@isep.ipp.pt>
2022-11-22 16:17:38 +0000

    LEI-78 #comment: UC L.1.3 - atualização do app.component e do routing

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarCamiaoComponent } from './criar-camiao/criar-camiao.component';
 import { MenuNavegacaoComponent } from './menu-navegacao/menu-navegacao.component';
-
+import { AppComponent } from './app.component';
 const routes: Routes = [
+  {path: 'appRoot', component: AppComponent},
   {path: 'adicionarCamiao', component: CriarCamiaoComponent},
   {path: 'menuNav', component: MenuNavegacaoComponent}
 ];
