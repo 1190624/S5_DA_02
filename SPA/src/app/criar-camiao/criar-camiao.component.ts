0d0ceffa4f5aee4b8d3e8f4d7250e559adfa3969
Lara Domingos <1190797@isep.ipp.pt>
2022-11-23 11:35:23 +0000

    LEI-78 #comment: UC L.1.3 - atualização da componente do camião

--- SPA/src/app/criar-camiao/criar-camiao.component.ts
+++ SPA/src/app/criar-camiao/criar-camiao.component.ts
@@ -12,77 +12,88 @@ import { ReturnStatement } from '@angular/compiler';
 })
 export class CriarCamiaoComponent implements OnInit {
   camiao: Camiao;
-  matricula: string; 
+  matricula: string;
   caracteristica: string;
-  autonomia:number;
-  capacidadeTransporte:number;
-  capacidadeBateria:number;
+  autonomia: number;
+  capacidadeTransporte: number;
+  capacidadeBateria: number;
   tara: number;
-  tempoCarregamento:string;
-  
-  
-  
-  constructor(private service : CamiaoService, private route: ActivatedRoute, private router:Router) { }
+  tempoCarregamento: string;
+
+
+
+  constructor(private service: CamiaoService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
   }
 
 
-    
-      Submit(): void {
 
-          this.camiao = new Camiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte, 
-            this.capacidadeBateria, this.tara, this.tempoCarregamento);
-          this.service.criarCamiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte, 
-            this.capacidadeBateria, this.tara, this.tempoCarregamento).subscribe(data => { alert("O camião foi criado.") });
-        /*
-        //Get inputs from the boxes
-        var matricula = document.getElementById('matricula') as HTMLInputElement; 
-        var caracteristica = document.getElementById('caracteristica') as HTMLInputElement;
-        var autonomia = document.getElementById('autonomia') as HTMLInputElement;
-        var capacidadeTransporte= document.getElementById('autonomia') as HTMLInputElement;
-        var capacidadeBateria= document.getElementById('capacidadeTransporte') as HTMLInputElement;
-        var tara= document.getElementById('tara') as HTMLInputElement;
-        var tempoCarregamento= document.getElementById('tempoCarregamento') as HTMLInputElement;
-
-  
-  /*
-          this.camiao = new Camiao(matricula.value, caracteristica.value, autonomia.valueAsNumber, capacidadeTransporte.valueAsNumber, 
-            capacidadeBateria.valueAsNumber, tara.valueAsNumber, tempoCarregamento.value);
-          this.service.criarCamiao(this.camiao).subscribe(data => { alert("O camião foi criado.") });
-          */
-           
-/*           this.service.criarCamiao(this.formBuilder.group({
-            matricula:matricula.value, 
-            caracteristica: caracteristica.value,
-            autonomia:autonomia.value,
-            capacidadeTransporte:capacidadeTransporte.value,
-            capacidadeBateria: capacidadeBateria.value,
-            tara:tara.value,
-            tempoCarregamento: tempoCarregamento.value
-          }).value).subscribe(data => { alert("O camião foi criado.") });
-    
-          //Cleaning input boxes after submission
-          title.value = '';
-          tags.value = '';
-          description.value = ''; */
-/*
-          matricula.value='';
-          caracteristica.value='';
-          autonomia.valueAsNumber=0;
-          capacidadeTransporte.valueAsNumber=0; 
-          capacidadeBateria.valueAsNumber=0;
-          tara.valueAsNumber=0;
-          tempoCarregamento.value='';
-         */ 
-          
-        }
-      
+  Submit(): void {
 
 
-      Return(): void{
-        this.router.navigate(['/appRoot']);
-      }
+    const MATRICULA_REGEX = new RegExp(/^[A-Z]{2}-[0-9]{2}-[A-Z]{2}$/);
+    //verificar se os valores do camião estão corretos.
+    if (this.autonomia < 90) {
+      alert("Automia do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 90).");
+    } else if (this.capacidadeBateria < 55) {
+      alert("Capacidade de Bateria do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 55).");
+    } else if (this.capacidadeTransporte < 800) {
+      alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
+    } else if (this.caracteristica == " ") {
+      alert("Necessário inserir uma Característica do Camião Elétrico!");
+    } else if (this.tara < 1000) {
+      alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
+    } else if (!MATRICULA_REGEX.test(this.matricula)) {
+      alert("Matrícula do Camião Elétrico não se apresenta no formato estipulado(formato exemplo = AA-00-ZZ).");
+    } else {
+
+      this.camiao = new Camiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte,
+        this.capacidadeBateria, this.tara, this.tempoCarregamento);
+      this.service.criarCamiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte,
+        this.capacidadeBateria, this.tara, this.tempoCarregamento).subscribe(data => { alert("O camião foi criado.") });
+    }
+
+
+
   }
 
 
+
+  Return(): void {
+    this.router.navigate(['/appRoot']);
+  }
+}
+
+
+
+
+/*
+//Get inputs from the boxes
+var matricula = document.getElementById('matricula') as HTMLInputElement; 
+var caracteristica = document.getElementById('caracteristica') as HTMLInputElement;
+var autonomia = document.getElementById('autonomia') as HTMLInputElement;
+var capacidadeTransporte= document.getElementById('autonomia') as HTMLInputElement;
+var capacidadeBateria= document.getElementById('capacidadeTransporte') as HTMLInputElement;
+var tara= document.getElementById('tara') as HTMLInputElement;
+var tempoCarregamento= document.getElementById('tempoCarregamento') as HTMLInputElement;
+ 
+/*           this.service.criarCamiao(this.formBuilder.group({
+  matricula:matricula.value, 
+  caracteristica: caracteristica.value,
+  autonomia:autonomia.value,
+  capacidadeTransporte:capacidadeTransporte.value,
+  capacidadeBateria: capacidadeBateria.value,
+  tara:tara.value,
+  tempoCarregamento: tempoCarregamento.value
+}).value).subscribe(data => { alert("O camião foi criado.") });
+ 
+/*
+matricula.value='';
+caracteristica.value='';
+autonomia.valueAsNumber=0;
+capacidadeTransporte.valueAsNumber=0; 
+capacidadeBateria.valueAsNumber=0;
+tara.valueAsNumber=0;
+tempoCarregamento.value='';
+*/ 
--- SPA/src/app/services/camiao.service.ts
+++ SPA/src/app/services/camiao.service.ts
@@ -14,15 +14,15 @@ export class CamiaoService {
   criarCamiao(matricula: string, caracteristica: string, autonomia:number, capacidadeTransporte:number, capacidadeBateria:number,
     tara: number, tempoCarregamento:string): Observable<any> {
 
-    this.verificarAtributos(matricula, caracteristica, autonomia, capacidadeTransporte, capacidadeBateria,tara, tempoCarregamento);
-    
-    const body = {"matricula":matricula,"caracteristica":caracteristica,
-    "autonomia":autonomia, "capacidadeTransporte":capacidadeTransporte, "capacidadeBateria":capacidadeBateria, "tara": tara, "tempoCarregamento":tempoCarregamento}
-    
-    console.log(body);
-    return this.httpClient.post(this.url, body).pipe(map(this.extractData));
+    //this.verificarAtributos(matricula, caracteristica, autonomia, capacidadeTransporte, capacidadeBateria,tara, tempoCarregamento);
+      const body = {"matricula":matricula,"caracteristica":caracteristica,
+      "autonomia":autonomia, "capacidadeTransporte":capacidadeTransporte, "capacidadeBateria":capacidadeBateria, "tara": tara, "tempoCarregamento":tempoCarregamento}
+      
+      console.log(body);
+      return this.httpClient.post(this.url, body).pipe(map(this.extractData));
+    }
 
-  }
+  
   
 /*
   atualizarCamio(matricula: string, caracteristica: string, autonomia:number, capacidadeTransporte:number, capacidadeBateria:number,
@@ -53,31 +53,7 @@ export class CamiaoService {
 
   verificarAtributos(matricula: string, caracteristica: string, autonomia:number, capacidadeTransporte:number, capacidadeBateria:number,
     tara: number, tempoCarregamento:string): void {
-      const MATRICULA_REGEX = new RegExp(/[A-Z]{2}-[0-9]{2}-[A-Z]{2}/);
-      //verificar se os valores do camião estão corretos.
-      if (autonomia < 90) {
-        alert("Automia do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 90).");
-      }
-  
-      if (capacidadeBateria < 55) {
-        alert("Capacidade de Bateria do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 55).");
-      }
-  
-      if (capacidadeTransporte < 800) {
-        alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
-      }
-  
-      if (caracteristica == " ") {
-        alert("Necessário inserir uma Característica do Camião Elétrico!");
-      }
-  
-      if (tara < 1000) {
-        alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
-      }
-  
-      if (!MATRICULA_REGEX.test(matricula)) {
-        alert("Matrícula do Camião Elétrico não se apresenta no formato estipulado(formato exemplo = AA-00-ZZ).");
-      }
+
   
 
   }
