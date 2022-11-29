317a696de170e8c5ad6dd4149b11025a01928583
Luís Carlos <1190818@isep.ipp.pt>
2022-11-29 18:10:44 +0000

    update camião component
    LEI-78 #comment Correção do camião component

--- SPA/src/app/criar-camiao/criar-camiao.component.ts
+++ SPA/src/app/criar-camiao/criar-camiao.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CamiaoService } from "src/app/services/camiao.service";
+import { CamiaoService } from "src/app/services/camiao/camiao.service";
 import { ActivatedRoute, Router } from '@angular/router';
 import { Camiao } from '../model/camiao';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -33,19 +33,22 @@ export class CriarCamiaoComponent implements OnInit {
 
 
     const MATRICULA_REGEX = new RegExp(/^[A-Z]{2}-[0-9]{2}-[A-Z]{2}$/);
-    //verificar se os valores do camião estão corretos.
-    if (this.autonomia < 90) {
-      alert("Automia do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 90).");
-    } else if (this.capacidadeBateria < 55) {
-      alert("Capacidade de Bateria do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 55).");
-    } else if (this.capacidadeTransporte < 800) {
-      alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
-    } else if (this.caracteristica == " ") {
-      alert("Necessário inserir uma Característica do Camião Elétrico!");
-    } else if (this.tara < 1000) {
-      alert("Capacidade de Transporte do Camião Elétrico é inferior ao minímo estipulado(valor mínimo = 800).");
-    } else if (!MATRICULA_REGEX.test(this.matricula)) {
-      alert("Matrícula do Camião Elétrico não se apresenta no formato estipulado(formato exemplo = AA-00-ZZ).");
+    //verificar se os valores do camião estão corretos.    
+    if (!MATRICULA_REGEX.test(this.matricula)) {
+      alert("Matrícula não é válida!\nFormato exemplo = AA-00-ZZ");    
+    } else if (this.caracteristica == null) {
+      alert("Necessário inserir uma Característica!");
+    } else if (this.autonomia < 90 || this.autonomia == null) {
+      alert("Automia não é válida!\nValor Mínimo = 90");    
+    } else if (this.capacidadeTransporte < 800 || this.capacidadeTransporte == null) {
+      alert("Capacidade de Transporte não é válida!\nValor Mínimo = 800");
+    } else if (this.capacidadeBateria < 55 || this.capacidadeBateria == null) {
+      alert("Capacidade de Bateria não é válida!\nValor Mínimo = 55");
+    } else if (this.tara < 1000 || this.tara == null) {
+      alert("Tara não é válida!\nValor Mínimo = 1000");
+    } else if (this.tempoCarregamento == null){
+      alert("Tempo de Carregamento não é válido")
+
     } else {
 
       this.camiao = new Camiao(this.matricula, this.caracteristica, this.autonomia, this.capacidadeTransporte,
