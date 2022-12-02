f82813247005828f60e10b43eb5924490f6f980a
Lara Domingos <1190797@isep.ipp.pt>
2022-12-02 22:04:53 +0000

    LEI-78 #comment: UC L.1.3 - atualização das classes listar camiões

---
+++ SPA/src/app/listar-camiao/listar-camiao.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Camiao } from '../model/camiao';
+import { CamiaoService } from '../services/camiao/camiao.service';
+
+@Component({
+  selector: 'app-listar-camiao',
+  templateUrl: './listar-camiao.component.html',
+  styleUrls: ['./listar-camiao.component.css']
+})
+export class ListarCamiaoComponent implements OnInit {
+  camiao: Camiao;
+  matricula: string;
+  caracteristica: string;
+  autonomia: number;
+  capacidadeTransporte: number;
+  capacidadeBateria: number;
+  tara: number;
+  tempoCarregamento: string;
+
+  constructor(private service: CamiaoService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.service.listaCamioes();
+  }
+}
