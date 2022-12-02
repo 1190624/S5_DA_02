503d43c571f074cce5f0e3113480db90fea5bdbf
1190825 Luís Costa <1190825@isep.ipp.pt>
2022-12-02 22:13:51 +0000

    Update app-routing.module.ts
    LEI-77 #comment modificação routing module para listar entregas

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { CriarEntregaComponent } from './criar-entrega/criar-entrega.component';
 import { MenuHeaderComponent } from './menu-header/menu-header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ListarCamiaoComponent } from './listar-camiao/listar-camiao.component';
+import { ListarEntregaComponent } from './listar-entrega/listar-entrega.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
@@ -15,7 +16,8 @@ const routes: Routes = [
   {path: 'adicionarArmazem', component: CriarArmazemComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'adicionarEntrega', component: CriarEntregaComponent},
-  {path: 'listarCamiao' , component: ListarCamiaoComponent}
+  {path: 'listarCamiao' , component: ListarCamiaoComponent},
+  {path: 'listarEntrega', component: ListarEntregaComponent}
 ];
 
 @NgModule({
