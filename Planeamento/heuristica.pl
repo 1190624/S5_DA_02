58c790202f047c013f71a653df349796375f3b52
Lara Domingos <1190797@isep.ipp.pt>
2022-12-04 20:22:10 +0000

    atualização da base de conehcimentos

--- Planeamento/heuristica.pl
+++ Planeamento/heuristica.pl
@@ -38,7 +38,6 @@ idArmazem('Espinho',2).
 idArmazem('Gondomar',3).
 idArmazem('Maia',4).
 idArmazem('Matosinhos',5).
-/*
 idArmazem('Oliveira de Azemeis',6).
 idArmazem('Paredes',7).
 idArmazem('Porto',8).
@@ -51,80 +50,209 @@ idArmazem('Vale de Cambra',14).
 idArmazem('Valongo',15).
 idArmazem('Vila do Conde',16).
 idArmazem('Vila Nova de Gaia',17).
-*/
+
 
 
 %distancia entre cidades
-%tmepo(armzem1, armazem2, distancia)
-distancia(1,2,122).
-distancia(1,3,122).
-distancia(1,4,151).
-distancia(1,5,147).
-/*
-distancia(1,6,74).
-distancia(1,7,116).
-distancia(1,8,141).
-distancia(1,9,185).
-distancia(1,10,97).
-distancia(1,11,164).
-distancia(1,12,76).
-distancia(1,13,174).
-distancia(1,14,59).
-distancia(1,15,132).
-distancia(1,16,181).
-distancia(1,17,128).
-*/
-
-distancia(2, 1,116).	
-distancia(2, 3,55).	
-distancia(2, 4,74).	
-distancia(2, 5,65).	
-/*
-distancia(2, 6,69).	
-distancia(2, 7,74).	
-distancia(2, 8,61).	
-distancia(2, 9,103).	
-distancia(2, 10,36).	
-distancia(2, 11,88).	
-distancia(2, 12,61).	
-distancia(2, 13,95).	
-distancia(2, 14,78).	
-distancia(2, 15,69).	
-distancia(2, 16,99).	
-distancia(2, 17,46).
-*/
-distancia(3,1,120).	
-distancia(3,2,50).		
-distancia(3,4,46).	
-distancia(3,5,46).	
+%distancia(armazem1, armazem2, distancia)
+distancia(1,2,53).
+distancia(1,3,57).
+distancia(1,4,67).
+distancia(1,5,65).
+distancia(1,6,30).
+distancia(1,7,44).
+distancia(1,8,57).
+distancia(1,9,92).
+distancia(1,10,37).
+distancia(1,11,80).
+distancia(1,12,29).
+distancia(1,13,83).
 
 
-distancia(4,1, 149).
-distancia(4,2,65).	
-distancia(4,3,46).	
-distancia(4,5,27).	
+distancia(2, 1,52).	
+distancia(2, 3,28).	
+distancia(2, 4,31).	
+distancia(2, 5,27).	
+distancia(2, 6,34).	
+distancia(2, 7,48).	
+distancia(2, 8,22).	
+distancia(2, 9,55).	
+distancia(2, 10,17).	
+distancia(2, 11,51).	
+distancia(2, 12,24).	
+distancia(2, 13,53).	
 
-distancia(5,1,141).
-distancia(5,2,55).
-distancia(5,3,48).
-distancia(5,4,25).
-/*distancia(5,6, 97).*/
 
+
+distancia(3,1,56).	
+distancia(3,2,28).		
+distancia(3,4,19).	
+distancia(3,5,18).	
+distancia(3, 6,46).	
+distancia(3, 7,29).	
+distancia(3, 8,10).	
+distancia(3, 9,45).	
+distancia(3, 10,35).	
+distancia(3, 11,34).	
+distancia(3, 12,40).	
+distancia(3, 13,36).	
+
+
+distancia(4,1,68).
+distancia(4,2,30).	
+distancia(4,3,20).	
+distancia(4,5,12).	
+distancia(4, 6,59).	
+distancia(4, 7,34).	
+distancia(4, 8,12).	
+distancia(4, 9,32).	
+distancia(4, 10,43).	
+distancia(4, 11,24).	
+distancia(4, 12,52).	
+distancia(4, 13,14).	
+
+
+distancia(5,1,64).
+distancia(5,2,25).
+distancia(5,3,18).
+distancia(5,4,11).
+distancia(5, 6,55).	
+distancia(5, 7,35).	
+distancia(5, 8,9).	
+distancia(5, 9,30).	
+distancia(5, 10,37).	
+distancia(5, 11,32).	
+distancia(5, 12,45).	
+distancia(5, 13,25).	
+
+
+distancia(6,1,29).
+distancia(6,2,34).
+distancia(6,3,47).
+distancia(6,4,58).	
+distancia(6, 5,55).	
+distancia(6, 7,60).	
+distancia(6, 8,48).	
+distancia(6, 9,83).	
+distancia(6, 10,18).	
+distancia(6, 11,70).	
+distancia(6, 12,11).
+distancia(6, 13,73).	
+
+
+distancia(7,1,45).
+distancia(7,2,48).
+distancia(7,3,28).
+distancia(7,4,32).
+distancia(7, 5,35).	
+distancia(7, 6,60).	
+distancia(7, 8,33).	
+distancia(7, 9,60).	
+distancia(7, 10,55).	
+distancia(7, 11,28).	
+distancia(7, 12,53).
+distancia(7, 13,39).	
+
+
+distancia(8,1,58).
+distancia(8,2,23).
+distancia(8,3,8).
+distancia(8,4,12).
+distancia(8, 5,9).	
+distancia(8, 6,48).	
+distancia(8, 7,32).	
+distancia(8, 9,37).	
+distancia(8, 10,32).	
+distancia(8, 11,28).	
+distancia(8, 12,42).
+distancia(8, 13,30).
+
+distancia(9,1,90).
+distancia(9,2,51).
+distancia(9,3,44).
+distancia(9,4,30).
+distancia(9, 5,29).	
+distancia(9, 6,81).	
+distancia(9, 7,59).		
+distancia(9, 8,35).	
+distancia(9, 10,64).	
+distancia(9, 11,36).	
+distancia(9, 12,71).	
+distancia(9, 13,24).	
+
+
+distancia(10,1,37).
+distancia(10,2,17).
+distancia(10,3,34).
+distancia(10,4,41).
+distancia(10, 5,38).	
+distancia(10, 6,18).	
+distancia(10, 7,53).	
+distancia(10, 8,30).		
+distancia(10, 9,65).	
+distancia(10, 11,57).	
+distancia(10, 12,8).	
+distancia(10, 13,58).
+
+
+distancia(11,1,81).
+distancia(11,2,51).
+distancia(11,3,35).
+distancia(11,4,22).
+distancia(11, 5,31).	
+distancia(11, 6,71).	
+distancia(11, 7,29).	
+distancia(11, 8,29).	
+distancia(11, 9,35).		
+distancia(11, 10,57).	
+distancia(11, 12,65).	
+distancia(11, 13,9).
+
+
+distancia(12,1,29).
+distancia(12,2,24).
+distancia(12,3,40).
+distancia(12,4,51).
+distancia(12,5,48).
+distancia(12, 6,10).	
+distancia(12, 7,53).	
+distancia(12, 8,41).	
+distancia(12, 9,76).	
+distancia(12, 10,8).	
+distancia(12, 11,63).		
+distancia(12, 13,66).	
+
+
+
+distancia(13,1,81).
+distancia(13,2,44).
+distancia(13,3,36).
+distancia(13,4,14).
+distancia(13,5,25).
+distancia(13, 6,71).	
+distancia(13, 7,38).	
+distancia(13, 8,25).	
+distancia(13, 9,25).	
+distancia(13, 10,59).	
+distancia(13, 11,9).	
+distancia(13, 12,65).	
 
 
 %entrega(<idEntrega>,<data>,<massaEntrefa>,<armazemEntrega>,<distanciaColoc>,<distanciaRet>)
 entrega(4439, 20221205, 200, 1, 8, 10).
 entrega(4439, 20221205, 100, 2, 8, 10).
-entrega(4439, 20221205, 400, 3, 8, 10).
+entrega(4439, 20221205, 401, 3, 8, 10).
 entrega(4439, 20221205, 500, 4, 8, 10).
-
-entrega(4439, 20221205, 200, 6, 8, 10).
-entrega(4439, 20221205, 200, 7, 8, 10).
-entrega(4438, 20221205, 150, 9, 7, 9).
-entrega(4445, 20221205, 100, 3, 5, 7).
-entrega(4443, 20221205, 120, 8, 6, 8).
-entrega(4449, 20221205, 300, 11, 15, 20).
-
+entrega(4439, 20221205, 110, 5, 8, 10).
+entrega(4439, 20221205, 150, 6, 8, 10).
+entrega(4438, 20221205, 300, 7, 7, 9).
+entrega(4445, 20221205, 299, 8, 5, 7).
+entrega(4443, 20221205, 120, 9, 6, 8).
+entrega(4449, 20221205, 410, 10, 15, 20).
+entrega(4449, 20221205, 20, 11, 15, 20).
+entrega(4449, 20221205, 500, 12, 15, 20).
+entrega(4449, 20221205, 501, 13, 15, 20).
+entrega(4449, 20221205, 600, 14, 15, 20).
 
 
 
@@ -200,11 +328,8 @@ bestfs3(ListaArmazens, Data, Cam, CaminhoFinal):-
     Novos),
 
 
-    %sort(+Key, +Order, +List, -Sorted)
+
     sort(0, @>=, Novos, NovosOrd),
-    %sort(Novos,NovosOrd),
-    %reverse(NovosOrd, NO),
-    %NO = [(_,[Atual|_])|_],
     NovosOrd = [(_,[Atual|_])|_],
     delete(ListaArmazens, Atual, ArmazensRestantes),
     bestfs3(ArmazensRestantes, Data, [Atual|Cam], CaminhoFinal).
